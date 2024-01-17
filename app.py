2556dd8d552cc5f0b57ccfb68ac84a2cd3eb022e
Glitch (funny-boiling-mortarboard) <none>
2024-01-17 14:15:09 +0000

    🕥🐠 Updated with Glitch

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
+import utils
 
 app = Flask(__name__)
 
-@app.route('/')
-def test():
-    return 'carlos'
+@app.route('/getB64/<string:x>', methods=['GET'])
+def getB64(x):
+  output = utils.binary_to_base64(x)
+  return jsonify(output)
+
+@app.route('/getB2from64/<string:x>', methods=['GET'])
+def getB2from64(x):
+  output = utils.base64_to_binary(x)
+  return jsonify(output)
+
+@app.route('/getB16/<string:x>', methods=['GET'])
+def getB16(x):
+  output = utils.base2_to_base16(x)
+  return jsonify(output)
+
+@app.route('/getB2from16/<string:x>', methods=['GET'])
+def getB2from16(x):
+  output = utils.base16_to_base2(x)
+  return jsonify(output)
